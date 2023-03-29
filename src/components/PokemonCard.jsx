0a772b415546940e0f8f818e20c74defe2db337e
K4iyzen <driss.khettab@gmail.com>
2023-03-29 16:29:20 +0200

    Création props

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,26 +1,13 @@
-const pokemonList = [
-    {
-      name: "bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "mew",
-    },
-  ];
 
-function PokemonCard () {
-    const pokemon = pokemonList[1];
-    console.log(pokemon)
 
-    
-
+function PokemonCard (props) {
+   // const pokemon = pokemonList[0]; 
+   console.log(props.pokemonList) 
     return(
-
-
+        
     <figure className="card">
-       {pokemon.imgSrc ? <img className="card-img" src={pokemon.imgSrc} alt ={pokemon.name}/> : <p>???</p>}
-        <figcaption>{pokemon.name}</figcaption>
+       {props.pokemonList.imgSrc ? <img className="card-img" src={props.pokemonList.imgSrc} alt ={props.pokemonList.name}/> : <p>???</p>}
+        <figcaption>{props.pokemonList.name}</figcaption>
     </figure>)
 
 };
